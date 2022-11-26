a58f5ba21f78d17471cbc7a25b9828b03ec926f4
Irthir <romain.schlotter@gmail.com>
2022-11-26 11:49:05 +0100

    nettoyage gitignore

--- GaCha7Equipe4/Intermediate/Build/Win64/GaCha7Equipe4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30147
-#include "D:/Data/GaCha/GaCha7Equipe4/Intermediate/Build/Win64/GaCha7Equipe4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- GaCha7Equipe4/Source/GaCha7Equipe4/GaCha7Equipe4.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GaCha7Equipe4.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GaCha7Equipe4, "GaCha7Equipe4" );
