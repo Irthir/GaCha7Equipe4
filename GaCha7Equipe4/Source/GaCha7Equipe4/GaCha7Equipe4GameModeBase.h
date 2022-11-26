b6870ae586cd57d9cc4aa0a3e43ecd38068346a1
Irthir <romain.schlotter@gmail.com>
2022-11-26 11:50:04 +0100

    nettoyage gitignore

---
+++ GaCha7Equipe4/Source/GaCha7Equipe4/GaCha7Equipe4GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GaCha7Equipe4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GACHA7EQUIPE4_API AGaCha7Equipe4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
